9226e255c0e6b7a8fcc7868f8563802cc0b9c6af
Nicolás Antinori <nicolas_antinori@hotmail.com>
2021-03-02 14:33:56 -0300

    Added redshift config in desktop work config

--- dotfiles/desktop_work_config/.config/bash-imports/aliases.sh
+++ dotfiles/desktop_work_config/.config/bash-imports/aliases.sh
@@ -1,4 +1,6 @@
 alias lg="lazygit"
 alias vim="nvim"
+alias vi="nvim"
 alias restart_network="systemctl restart network-manager"
 alias ifconfig="ip a"
+alias ports-in-use='sudo lsof -i -P -n | grep LISTEN'
