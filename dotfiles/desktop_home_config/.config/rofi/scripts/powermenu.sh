d267e0f57a3ac094948dfce40ab240429d1ef402
Nicolás Antinori <nantinori@findo.com.ar>
2021-07-13 17:51:46 -0300

    Fixed powermenu

--- dotfiles/desktop_home_config/.config/rofi/scripts/powermenu.sh
+++ dotfiles/desktop_home_config/.config/rofi/scripts/powermenu.sh
@@ -1,32 +1,33 @@
-#!/bin/bash
 options="
 
 
 "
 themes_dir=$HOME/.config/rofi/themes
 theme=${1:-$themes_dir/powermenu.rasi}
-selection=$(echo -e "${options}" | rofi -dmenu -p -theme $theme -normal-window)
+selection=$(echo "${options}" | rofi -dmenu -p -theme $theme -normal-window)
 
 echo "This is your selection: $selection"
 
 case "${selection}" in
     "")
         $HOME/.config/rofi/scripts/closeapps.sh
-	rofi -e "Shuting down..." -theme $theme -normal-window &
+        rofi -e "Shuting down..." -theme $theme -normal-window &
         sleep 3
         systemctl poweroff -i
       ;;
     "")
         $HOME/.config/rofi/scripts/closeapps.sh
-	rofi -e "Rebooting..." -theme $theme -normal-window &
+        rofi -e "Rebooting..." -theme $theme -normal-window &
         sleep 3
         systemctl reboot
       ;;
     "")
         $HOME/.config/rofi/scripts/closeapps.sh
-	rofi -e "Logging out..." -theme $theme -normal-window &
+        rofi -e "Logging out..." -theme $theme -normal-window &
         sleep 3
-        i3 exit;;
+        i3 exit
+      ;;
     "")
-        $HOME/.config/i3/lock_and_blur.sh;;
+        dbus-send --type=method_call --dest=org.gnome.ScreenSaver  /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
+      ;;
 esac
